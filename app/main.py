f9cf45585d371a5df26193014a94fb45859cff41
BhardwajArjit <iamarjitbhardwaj@gmail.com>
2023-01-27 16:59:26 +0530

    Added the functionality of deleting and updating post

--- app/main.py
+++ app/main.py
@@ -24,6 +24,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(myPosts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
     return {"message": "Hare Krishna"}
@@ -51,3 +57,26 @@ def getPost(id: int):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"message": f"Post with id: {id} was not found"}
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def deletePost(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+    myPosts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def updatePost(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    myPosts[index] = post_dict
+    return {'data': post_dict}
